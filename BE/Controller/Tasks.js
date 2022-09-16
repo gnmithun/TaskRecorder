f4faa353f5372aae907cdbde0c8813190d37db12
Mithun Nair <gn.mithun@gmail.com>
2022-09-16 19:24:20 +0530

    Error Handlers

--- BE/Controller/Tasks.js
+++ BE/Controller/Tasks.js
@@ -1,5 +1,6 @@
 const Tasks = require("../Model/Tasks")
 const { taskValidator } = require("../Common/validator")
+const Category = require("../Model/Category")
 
 exports.createTask = async (req,res) => {
   const { error, value } = taskValidator.validate( { detail : req.body.detail , completed : req.body.completed, category: req.body.category } )
@@ -7,11 +8,21 @@ exports.createTask = async (req,res) => {
     return res.status(200).send( { "response" : "Error", "details" : error } )
   }
   try {
-    const newTask = await Tasks.create( { detail:value.detail, completed:value.completed, categoryId:value.category  } )
+    const newTask = await Tasks.create( { detail:value.detail, completed:value.completed, categoryId:value.category } )
     return res.status(200).send( { "response" : "Success", "details" : newTask.id } )
   } catch (error) {
     return res.status(200).send( { "response" : "Error", "details" : error } )
   }
-  
+}
+
+exports.getTasks = async ( req,res ) => {
+  const tasks = await Tasks.findAll({ include: Category })
+  console.log(JSON.stringify(tasks, null, 1));
+  try {
+    const tasks = await Tasks.findAll()
+      return res.status(200).send( { "response" : "Success", "tasks" : tasks } )
+  } catch (error) {
+      return res.status(200).send( { "response" : "Error", "error" : error } )
+  } 
 }
 
--- BE/Router/Tasks.js
+++ BE/Router/Tasks.js
@@ -3,6 +3,7 @@ const Router = express.Router()
 const TaskController = require('../Controller/Tasks')
 
 Router.post('/tasks',TaskController.createTask)
+Router.get('/tasks',TaskController.getTasks)
 
 module.exports = Router
 
