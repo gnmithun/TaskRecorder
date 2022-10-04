6d400c0548287a7f1012be768923e427f1225b06
Mithun Nair <gn.mithun@gmail.com>
2022-10-04 16:11:58 +0530

    Added activity Indicator

--- client/src/TaskManager.js
+++ client/src/TaskManager.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Tasks from "./Tasks";
 function TaskManager(props) {
     return(
--- client/src/Tasks.js
+++ client/src/Tasks.js
@@ -1,6 +1,7 @@
 
 import React, {useState } from 'react';
 import TasksList from './TasksList';
+import LoadingSpinner from "./LoadingSpinner";
 
 function Tasks(props) {
 
@@ -8,6 +9,7 @@ function Tasks(props) {
         setTask(task => ( {...task,[event.target.name] : event.target.value } ) ) 
     }
 
+    const [loading,setLoading] = useState(false)
     const [tasks,setTasks] = useState([])
     const [task,setTask] = useState({
         detail:"",
@@ -23,31 +25,36 @@ function Tasks(props) {
             body: JSON.stringify({ detail:task.detail, completed:task.completed,categoryId:2}),
             mode:'cors',
         }
+
+        setLoading(true)
         const resp = await fetch('http://localhost:8000/tasks',requestOptions)
         const data = await resp.json()
         const newTask = data.details
         setTask(newTask)
         setTasks(tasks => [...tasks,newTask])
-
+        setLoading(false)
     }
 
     return (
         <div>
+            { loading ? <LoadingSpinner/> : <></>}
             <form onSubmit={addTask}>
                 <input  type="text" 
-                        value={ task.detail } 
+                        value={ task.detail }                        
                         placeholder='What do you want today?' 
+                        disabled = { loading ? true : false }
                         name='detail' 
                         onChange={ onChange }/>
 
                 <input type="checkbox" 
-                       value={ task.completed } 
+                       value={ task.completed }
+                       disabled = { loading ? true : false }
                         name='completed' 
                     onChange={ (event) => { 
                             setTask(task => ( {...task,completed :  event.target.checked } ) )
                         }
                     } />
-                <input type="submit" value="Submit"/>
+                <input type="submit" value="Submit" disabled = { loading ? true : false }/>
                 <TasksList  value={tasks}/>
             </form>
               <h6> Results </h6>
