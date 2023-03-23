4913fab1c77ab47a21ec6b322bb124686f94cd59
Mithun Nair <gn.mithun@gmail.com>
2023-03-23 15:50:39 +0530

    SignIn

--- server/Controller/Access.js
+++ server/Controller/Access.js
@@ -1,35 +1,46 @@
 const hasher = require('password-hash')
 const { Users }  = require('../Model/Users')
+const jwt = require('jsonwebtoken')
 
 exports.signup = async (req,res,next) => {
   
     try {
         const email = req.body.email
-        const password = req.body.password
-        
-        const user = await Users.findOne({
-          where: {
-            email : email
-          }            
-        })
+        const password = req.body.password 
+        const user = await Users.findOne( {where: { email : email } } )
         if ( user !== null ) {
             return res.send( { "response" : "Success", "details" :  " User already exists! " } )
         } else {
-            const hashPassword = hasher.generate(req.body.password)            
-            const newTask = await Users.create( { email:email, password:hashPassword } )
-            return res.send( { "response" : "Success", "details" : { "email" : req.body.email } } )
+            const hashPassword = hasher.generate(password)            
+            const user = await Users.create( { email:email, password:hashPassword } )
+            return res.send( { "response" : "Success", "details" : { "email" : user.email } } )
         }
     } catch (error){
         next(error)
     }
 }
 
-exports.signin = (req,res,next) => {
+exports.signin = async (req,res,next) => {
     try {
-        const hashedPassword = "sha1$0e8d9895$1$8f2a1bed89e032ae8f906a27546731263ee5d041"
-        const isPasswordCorrect = hasher.verify(req.body.password,hashedPassword)
-        return res.send( { "response" : "Success", "details" : { "email" : req.body.email, "password" : req.body.password, "isPasswordVerified" : isPasswordCorrect } } )
+        const email = req.body.email
+        const password = req.body.password
+        
+        const user = await Users.findOne( { where : { email : email } } )
+        if ( user !== null ) {
+            const hashedPassword = user.password
+            const isPasswordCorrect = hasher.verify(password,hashedPassword)
+
+            if (isPasswordCorrect) {
+                const token = jwt.sign({ email : email },"E(H+MbQeShVmYq3t6w9z$C&F)J@NcRfU")
+                return res.send( { "response" : "Success", "details" : "Logged In", "token" : token } )
+            }
+            else{
+                return res.send( { "response" : "Success", "details" : "Password Incorrect!!" } )
+            }
+        } else {
+            return res.send( { "response" : "Success", "details" : "User not found!!" } )
+        }
     } catch (error){
-        next()
+        next(error)
     }
 }
