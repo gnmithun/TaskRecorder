73075d8a7a5f67182e171cdf2798a942adccde9e
Mithun Nair <gn.mithun@gmail.com>
2023-04-27 15:43:38 +0530

    Minor change

--- client/src/Common/customFetch.js
+++ client/src/Common/customFetch.js
@@ -2,7 +2,6 @@ export const customFetch = async (endpoint,options) => {
     const defaultHeaders = {
         headers: {'Content-Type': 'application/json'},
         credentials:"include",                                              
-        mode:'cors'
     }
     const result = await fetch(endpoint,{...options,...defaultHeaders})
     return result
