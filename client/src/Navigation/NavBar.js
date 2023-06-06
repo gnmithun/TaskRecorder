ec861fad4a6fafa238c9a683528d22c1f6ad3af8
Mithun Nair <gn.mithun@gmail.com>
2023-06-06 13:11:27 +0530

    Added CSS

--- client/src/Navigation/NavBar.js
+++ client/src/Navigation/NavBar.js
@@ -1,30 +1,45 @@
 import React from "react"
-import navigationStyles from './Navigation.module.css'
-import styles from '../TaskManager/TaskManager.module.css'
+import "./NavBar.css"
+// export default function NavBar(props){    
+//     return(
+//         <div className={ navigationStyles.navigationMain }>
+//         <div className={ navigationStyles.dropDown}> 
+//           <ul className={ navigationStyles.navigationItems}> 
+//              About
+//           </ul>
+//           <div className= { navigationStyles.dropDownContent } >
+//             <p className={ navigationStyles.clickableMenuItem }> About </p>
+//             <hr className= { styles.hrCustom }></hr>
+//             <p className={ navigationStyles.clickableMenuItem }> T & C </p>
+//             <hr className= { styles.hrCustom }></hr>
+//             <p className={ navigationStyles.clickableMenuItem } onClick = { props.signout }> Signout </p>
+//           </div>
+//         </div>
+//         <div className={ navigationStyles.dropDown}> 
+//           <ul className = { navigationStyles.navigationItems } > 
+//             Category 
+//           </ul>
+//           <div className = {navigationStyles.dropDownContent } >
+//             <p className={ navigationStyles.clickableMenuItem } onClick={ props.addCategory }> + Category </p>
+//           </div>
+//         </div>
+//       </div>
+//     )
+// }
 
-export default function NavBar(props){    
-    return(
-        <div className={ navigationStyles.navigationMain }>
-        <div className={ navigationStyles.dropDown}> 
-          <ul className={ navigationStyles.navigationItems}> 
-             About
-          </ul>
-          <div className= { navigationStyles.dropDownContent } >
-            <p className={ navigationStyles.clickableMenuItem }> About </p>
-            <hr className= { styles.hrCustom }></hr>
-            <p className={ navigationStyles.clickableMenuItem }> T & C </p>
-            <hr className= { styles.hrCustom }></hr>
-            <p className={ navigationStyles.clickableMenuItem } onClick = { props.signout }> Signout </p>
-          </div>
-        </div>
-        <div className={ navigationStyles.dropDown}> 
-          <ul className = { navigationStyles.navigationItems } > 
-            Category 
-          </ul>
-          <div className = {navigationStyles.dropDownContent } >
-            <p className={ navigationStyles.clickableMenuItem } onClick={ props.addCategory }> + Category </p>
-          </div>
-        </div>
+export default function NavBar(props) {
+  return (
+    <div>
+      <div className="navigationMain">
+        <p> One Item </p>
+        <p> Another Item </p>
+        <ul>
+          <li> <a href="#"> Home  </a> </li>
+          <li> <a href="#"> About </a> </li>
+          <li> <a href="#"> FAQ   </a> </li>
+        </ul>
       </div>
-    )
+    </div>
+  );
 }
+
